be1302ba4ff51436521828648bae1c223a65f231
Joshua Wilding <josh@Joshuas-MacBook-Pro.local>
2017-10-30 14:33:21 -0600

    Updated lobbyServer class to have some basic information available. Still needs better definition of our types of messages.

---
+++ lobbyServer/lobbyServer.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using serverMessage.serverMessage;
+namespace lobbyServer
+{
+    public class clientPlayer
+    {
+        public String playerName 
+        {
+            get
+            {
+                return playerName;
+            }
+            set
+            {
+                playerName = value;
+            }
+        
+        }
+        public clientPlayer( serverMessage initialClientMessage )
+        {
+            //extract the player name
+            //set the value of variable playerName
+            //to the input value
+        }
+        public bool hasJoinedGame 
+        {
+            get
+            {
+                return hasJoinedGame;
+            }
+            set
+            {
+                hasJoinedGame = value;
+            }
+        }
+        public bool playerHasRequestedJoin 
+        {
+            get 
+            {
+                return playerHasRequestedJoin;
+            }
+            set
+            {
+                playerHasRequestedJoin = value;
+            }
+        }
+        public bool playerHasRequestedLeave 
+        {
+            get
+            {
+                return playerHasRequestedLeave;
+            }
+            set
+            {
+                playerHasRequestedLeave = value;
+            }
+        }
+        public serverMessage SendIsReady() 
+        {
+            //create an isReady message
+            //send an isReady message
+        }
+    }
+    public class lobbyGame
+    {
+        public clientPlayer[] associatedPlayers;
+        public bool isActive;
+        public bool isOpen;
+        public lobbyGame()
+        {
+            associatedPlayers = null;
+            isActive = false;
+            isOpen = true;
+        }
+        public void playerJoinGame( clientPlayer newPlayer ) 
+        {
+            foreach( clientPlayer p in associatedPlayers )
+            {
+                if (newPlayer == p)
+                {
+                    return; //player is already in game
+                }
+            }
+            associatedPlayers.SetValue(newPlayer, associatedPlayers.GetUpperBound(1) + 1);
+        }
+    }
+    public class lobbyServer
+    {
+        public clientPlayer[] totalAssociatedPlayers;
+        public lobbyGame[] gamesInLobby;
+        public void newLobbyPlayer(clientPlayer newPlayer)
+        {
+            foreach ( clientPlayer p in totalAssociatedPlayers )
+            {
+                if ( newPlayer == p )
+                {
+                    return; //The player already exists
+                }
+            }
+            totalAssociatedPlayers.SetValue( newPlayer, totalAssociatedPlayers.GetUpperBound(1) + 1);
+        }
+            
+        public lobbyServer()
+        {
+            totalAssociatedPlayers = null;
+            //Create two new instances of lobbygame
+            lobbyGame firstGame = new lobbyGame();
+            lobbyGame secondGame = new lobbyGame());
+            gamesInLobby.SetValue(firstGame, gamesInLobby.GetUpperBound(1) + 1);
+            gamesInLobby.SetValue(secondGame, gamesInLobby.GetUpperBound(1) + 1);
+        }
+    }
+}
---
+++ serverMessage/serverMessage/serverMessage.cs
@@ -0,0 +1,67 @@
+﻿using System;
+// refer to : https://msdn.microsoft.com/en-us/library/system.runtime.serialization.json(v=vs.110).aspx
+//            https://docs.microsoft.com/en-us/dotnet/api/system.runtime.serialization.json?view=netframework-4.7.1
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
+
+namespace serverMessage
+{
+	//construct a data contract here
+	//https://docs.microsoft.com/en-us/dotnet/api/system.runtime.serialization.datacontractattribute?view=netframework-4.7.1
+    [DataContract(Name = "serverMessage", Namespace = "serverMessage" )]
+    public class serverMessage : IExtensibleDataObject 
+    {
+        [DataMember(Name = "messageID")]
+		public int messageID;
+        [DataMember(Name = "conversationID")]
+		public int conversationID;
+        [DataMember(Name = "messageType")]
+		public String messageType;
+        [DataMember(Name = "messageBody")]
+		public String messageBody;
+        [DataMember(Name = "sentAs")]
+		public String sentAs;
+        [DataMember(Name = "initiator")]
+		public String initiator;
+
+
+		public serverMessage(int inputMessageID, int inputConversationID, String inputMessageType,
+							 String inputMessageBody, String inputSentAs, String inputInitiator)
+		{
+			messageID = inputMessageID;
+			conversationID = inputConversationID;
+			messageType = inputMessageType;
+			messageBody = inputMessageBody;
+			sentAs = inputSentAs;
+			initiator = inputInitiator;
+		}
+
+        private ExtensionDataObject messageDataValue;
+        public ExtensionDataObject ExtensionData {
+            get
+            {
+                return messageDataValue;
+            }
+            set
+            {
+                messageDataValue = value;
+            }
+        }
+    }
+
+    public class serverMessageRW
+    {
+        public serverMessageRW( byte[] encodedMessage ) 
+        {
+            //get string from byte[]
+            //parse JSON
+        }
+
+        byte[] EncodeMessage()
+        {
+            //encode JSON
+            //change JSON string to byte[]
+            //return byte[]
+        }
+    }
+}
